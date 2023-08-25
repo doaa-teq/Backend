d3491b2d0b237ab9e27efd82ef2cf879473d3959
doaa-teq <84857602+doaa-teq@users.noreply.github.com>
2023-08-25 17:13:56 +0300

    Add files via upload

---
+++ if_condition.php
@@ -0,0 +1,13 @@
+<?php
+$num=-6;
+if ($num<0){
+    echo "number is negative <br>";
+}
+elseif ($num > 0){
+    echo "the number is positive <br>";
+}
+else{
+    echo " the number is equal to zero <br>";  
+}
+echo $Number = is_numeric($num);
+?>
